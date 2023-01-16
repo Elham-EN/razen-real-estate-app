78817444870c4a83917b608fdbd13e3c5c72d254
Elham-EN <ilhamrezaie@gmail.com>
2023-01-16 19:55:26 +1100

    created pages & routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Import pages component
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+
 function App() {
   return (
-    <h1 className="text-6xl text-center font-extralight mt-auto">
-      Hello world!
-    </h1>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
