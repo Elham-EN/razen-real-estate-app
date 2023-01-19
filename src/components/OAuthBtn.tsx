30d61991e24561de2a2c0146d039caba96b6e7c3
Elham-EN <ilhamrezaie@gmail.com>
2023-01-20 00:49:37 +1100

    Integrated Google OAuth to the React app

--- src/components/OAuthBtn.tsx
+++ src/components/OAuthBtn.tsx
@@ -1,7 +1,20 @@
+import { MouseEvent } from "react";
+import useAuth from "../hooks/useAuth";
 import { FcGoogle } from "react-icons/fc";
+
 export default function OAuthBtn(): JSX.Element {
+  const { googleAuth } = useAuth();
+  const onGoogleClick = async (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    await googleAuth();
+  };
+
   return (
-    <button className="flex items-center justify-center bg-blue-600 py-3 text-white transition duration-300 ease-in-out hover:bg-blue-700 font-medium shadow-md hover:shadow-lg active:shadow-lg rounded">
+    <button
+      type="button"
+      onClick={onGoogleClick}
+      className="flex items-center justify-center bg-blue-600 py-3 text-white transition duration-300 ease-in-out hover:bg-blue-700 font-medium shadow-md hover:shadow-lg active:shadow-lg rounded"
+    >
       <FcGoogle className="mr-3 text-2xl bg-white rounded-full" />
       <p className="uppercase">continue with google</p>
     </button>
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,17 +3,16 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   updateProfile,
-  User,
   AuthError,
+  GoogleAuthProvider,
+  signInWithPopup,
 } from "firebase/auth";
 import { useNavigate } from "react-router";
-import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import { auth, firestoreDB } from "../firebase.config";
 import { toast } from "react-toastify";
 
 export default function useAuth() {
-  //   const [user, setUser] = useState<User | null>(null);
-  // const [errorExist, setErrorExist] = useState<boolean>(false);
   const [isPending, setIsPending] = useState<boolean>(false);
   const navigateTo = useNavigate();
 
@@ -37,12 +36,44 @@ export default function useAuth() {
         timestamp,
       });
       toast.success("User sign up successfully");
-      navigateTo("/");
-      setIsPending(false);
+      setTimeout(() => {
+        setIsPending(false);
+        navigateTo("/");
+      }, 5000);
     } catch (err) {
       const authError = err as AuthError;
       toast.error(authError.message);
       setIsPending(false);
+    }
+  };
+
+  const googleAuth = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const userCredential = await signInWithPopup(auth, provider);
+      const { displayName, email, uid } = userCredential.user;
+      const timestamp = serverTimestamp();
+
+      // Check if the user does exist in the user collections
+      const docRef = doc(firestoreDB, "users", uid);
+      const docSnapshot = await getDoc(docRef);
+      if (!docSnapshot.exists()) {
+        // Store user credential info into Firestore Database in users collection
+        await setDoc(doc(firestoreDB, "users", uid), {
+          fullname: displayName,
+          email,
+          timestamp,
+        });
+        toast.success("User sign up successfully");
+        setTimeout(() => {
+          setIsPending(false);
+          navigateTo("/");
+        }, 5000);
+      } else {
+        toast.error("User already signed up");
+      }
+    } catch (error) {
+      toast.error("Could not authorize with Google Server");
     }
   };
 
@@ -54,5 +85,5 @@ export default function useAuth() {
     }
   };
 
-  return { signUp, signIn, isPending };
+  return { signUp, signIn, googleAuth, isPending };
 }
