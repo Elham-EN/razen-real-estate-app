e4e54a739e7af10ca311cda358578dd3c8a67caa
Elham-EN <ilhamrezaie@gmail.com>
2023-01-19 14:17:33 +1100

    Configured firebase

---
+++ src/firebase.config.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// Access Firebase services
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY as string,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN as string,
+  projectId: import.meta.env.VITE_PROJECT_ID as string,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET as string,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID as string,
+  appId: import.meta.env.VITE_APP_ID as string,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const firebaseAuth = getAuth(app);
+export const firestoreDB = getFirestore(app);
+export const firebaseStorage = getStorage(app);
